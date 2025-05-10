9611de105a300062b36458988f50d5e65306076f
AgusS05 <137123246+AgusS05@users.noreply.github.com>
2025-05-10 14:24:11 -0300

    Add files via upload

---
+++ 05 Listas/Practico_5_Listas.py
@@ -0,0 +1,75 @@
+#1
+"""
+multiplos_de_4 = list(range(4, 101, 4))
+print(multiplos_de_4)
+"""
+
+#2
+"""
+vector = [1, 2, 3, 4, 5]
+penultimo = vector[3]
+print(penultimo)
+"""
+
+#3
+"""
+lista1 = []
+lista1.append("Hola")
+lista1.append("5")
+lista1.append("7")
+print(lista1)
+"""
+
+#4
+"""
+animales = ["perro", "gato", "conejo", "pez"] 
+animales[1] = "loro"
+animales[3] = "oso"
+print(animales)
+"""
+
+#5
+"""
+#Este programa elimina el número más grande de la lista.
+
+numeros = [8, 15, 3, 22, 7] 
+numeros.remove(max(numeros))
+print(numeros)
+"""
+
+#6
+"""
+lista = list(range(10, 31, 5))
+print(lista[:2])
+"""
+
+#7
+"""
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "voyage"
+autos[2] = "taos"
+print(autos)
+"""
+#8
+"""
+dobles = []
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print(dobles)
+"""
+#9
+"""
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], 
+["agua"]] 
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+"""
+
+#10
+"""
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print(lista_anidada)
+"""
